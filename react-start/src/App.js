48d0ba80af1ad48808fd94a868fb785fdc1c8ab9
iryna18artemenko <136636205+iryna18artemenko@users.noreply.github.com>
2023-10-25 16:25:47 +0300

    home.work40

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -5,19 +5,19 @@ import './App.css';
 
 function App() {
   const [smiles, setSmiles] = useState([ 
-  {id: 0, name: "Smile 1", counter: 0, src: "images/smail1.jpg", alt:"smile1"},
-  {id: 1, name: "Smile 2", counter: 0, src: "images/smail2.jpg", alt:"smile2"},
-  {id: 2, name: "Smile 3", counter: 0, src: "images/smail3.jpg", alt:"smile3"},
-  {id: 3, name: "Smile 4", counter: 0, src: "images/smail4.jpg", alt:"smile4"},
-  {id: 4, name: "Smile 5", counter: 0, src: "images/smail5.jpg", alt:"smile5"}]);
+  {id: 0, name: "Smile 1", counter: 0, src: "images/smail0.jpg", alt:"smile0"},
+  {id: 1, name: "Smile 2", counter: 0, src: "images/smail1.jpg", alt:"smile1"},
+  {id: 2, name: "Smile 3", counter: 0, src: "images/smail2.jpg", alt:"smile2"},
+  {id: 3, name: "Smile 4", counter: 0, src: "images/smail3.jpg", alt:"smile3"},
+  {id: 4, name: "Smile 5", counter: 0, src: "images/smail4.jpg", alt:"smile4"}]);
 
   const [winner, setWinner] = useState(-1);
   
 
   function findWinner () {
-    const winNumber = Math.max(...smiles.map(smile => smile.counter));
-    const winnerId = smiles.indexOf(winNumber);
-    setWinner(winnerId);
+    const winSmile = Math.max(...smiles.map(smile => smile.counter));
+    const winnerId = smiles.find(smile => smile.counter === winSmile);
+    setWinner(winnerId.id);
   };
 
   function hendelClick (id) {
@@ -35,15 +35,15 @@ function App() {
       <h1>Smile Competition</h1>
       <div style={{display:"flex"}}>
         {winner > -1 ? (
-         <> 
+         <div className='images'> 
             <h2>Winner:</h2>
             <img className='img'  src={`images/smail${winner}.jpg`} alt={`smail${winner}`} /> 
-         </>
+         </div>
          ) : smiles.map((smile, id)=>( 
             <div className='images' key={id}>
                 <p>{smile.name}</p>
-               <img className='img' style={{cursor: "pointer"}} onClick={() => hendelClick(id)} src={smile.src} alt={smile.alt} />
-               <p>Count {smile.counter}</p>
+                <img className='img' onClick={() => hendelClick(id)} src={smile.src} alt={smile.alt} />
+                <p>Count {smile.counter}</p>
             </div>))}
       </div>
        {winner < 0 ? (<button onClick={() => findWinner()}>Show winner</button>) : null}
