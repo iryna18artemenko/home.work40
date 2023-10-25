e5846e5e1feb082af47ee52b0b40451c6eb2dea7
iryna18artemenko <136636205+iryna18artemenko@users.noreply.github.com>
2023-10-25 15:51:37 +0300

    home.work40

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,48 +1,56 @@
 import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [smiles, setSmiles] = useState([ 
+  {id: 0, name: "Smile 1", counter: 0, src: "images/smail1.jpg", alt:"smile1"},
+  {id: 1, name: "Smile 2", counter: 0, src: "images/smail2.jpg", alt:"smile2"},
+  {id: 2, name: "Smile 3", counter: 0, src: "images/smail3.jpg", alt:"smile3"},
+  {id: 3, name: "Smile 4", counter: 0, src: "images/smail4.jpg", alt:"smile4"},
+  {id: 4, name: "Smile 5", counter: 0, src: "images/smail5.jpg", alt:"smile5"}]);
 
-  const [value, setValue] = useState(0);
+  const [winner, setWinner] = useState(-1);
+  
 
-  function clickImg () {
-    setValue (prevState => prevState + 1)
+  function findWinner () {
+    const winNumber = Math.max(...smiles.map(smile => smile.counter));
+    const winnerId = smiles.indexOf(winNumber);
+    setWinner(winnerId);
   };
 
-  function result () {
-  
-        console.log (Math.max(value))
-  
-  }
+  function hendelClick (id) {
+
+    setSmiles(smiles.filter (smile => {
+      if (smile.id === id) {
+        smile.counter++
+      } 
+      return smile;}))
+  };
+
 
   return (
-    <div className="App">
-      <div className="conteiner">
-          <div className="images">
-            <img id="img1" className="img" src="images/smail1.jpg" alt="smail1" onClick = {clickImg}/>
-            <div id="num1" className="num">{value}</div>
-          </div>
-          <div className="images">
-            <img id="img2" className="img" src="images/smail2.jpg" alt="smail2" onClick = {clickImg}/>
-            <div id="num2" className="num">{value}</div>
-          </div>
-          <div className="images">
-            <img id="img3" className="img" src="images/smail3.jpg" alt="smail3" onClick = {clickImg}/>
-            <div id="num3" className="num">{value}</div>
-          </div>
-          <div className="images">
-            <img id="img4" className="img" src="images/smail4.jpg" alt="smail4" onClick = {clickImg}/>
-            <div id="num4" className="num">{value}</div>
-          </div>
-          <div className="images">
-            <img id="img5" className="img" src="images/smail5.jpg" alt="smail5" onClick = {clickImg}/>
-            <div id="num5" className="num">{value}</div>
-          </div>
-       </div>
-          <button className="btn" onClick={result} >Show results</button>
-          
+    <div>
+      <h1>Smile Competition</h1>
+      <div style={{display:"flex"}}>
+        {winner > -1 ? (
+         <> 
+            <h2>Winner:</h2>
+            <img className='img'  src={`images/smail${winner}.jpg`} alt={`smail${winner}`} /> 
+         </>
+         ) : smiles.map((smile, id)=>( 
+            <div className='images' key={id}>
+                <p>{smile.name}</p>
+               <img className='img' style={{cursor: "pointer"}} onClick={() => hendelClick(id)} src={smile.src} alt={smile.alt} />
+               <p>Count {smile.counter}</p>
+            </div>))}
+      </div>
+       {winner < 0 ? (<button onClick={() => findWinner()}>Show winner</button>) : null}
     </div>
+   
   );
 }
+
 
 export default App;
